007c106ae754cbe6c44b07e059522ca3464187ac
Mateusz Suchorab <suchy1122@poczta.fm>
2017-11-16 11:11:36 +0100

    Dodanie zadan z GUI

---
+++ Gui/src/gui/Gui.java
@@ -0,0 +1,17 @@
+package gui;
+
+public class Gui {
+
+    public static void main(String[] args) {
+
+        //HelloWindow helloWindow = new HelloWindow();
+        //helloWindow.setVisible(true);
+        //CalculatorWindow calculatorWindow = new CalculatorWindow();
+        //calculatorWindow.setVisible(true);
+        //RunningButton runningButton = new RunningButton();
+        //runningButton.setVisible(true);
+        //CheckYourPESEL pesel = new CheckYourPESEL();
+        //pesel.setVisible(true);
+    }
+
+}
